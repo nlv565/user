29072fa2f8bac4537925213d0578d50422052bda
nlv565 <vannguyen3807@gmail.com>
2023-10-06 16:40:09 +0700

    ..

---
+++ User/src/view/Main.java
@@ -0,0 +1,27 @@
+
+package view;
+
+import Controller.Manager;
+
+/**
+ *
+ * @author COMPUTER
+ */
+public class Main {
+      public static void main(String[] args) {
+        //loop until user want to exit
+        while (true) {
+            int choice = Manager.menu();
+            switch (choice) {
+                case 1:
+                    Manager.createNewAccount();
+                    break;
+                case 2:
+                    Manager.loginSystem();
+                    break;
+                case 3:
+                    return;
+            }
+        }
+    }
+}
